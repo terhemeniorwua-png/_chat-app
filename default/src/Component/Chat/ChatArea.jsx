bb5885f9bd80363d0adeb66251e3d4313e011bc2
PHILIP <terhemeniorwua@gmail.com>
2026-08-16 15:07:34 +0100

    edit the chatArea page

--- default/src/Component/Chat/ChatArea.jsx
+++ default/src/Component/Chat/ChatArea.jsx
@@ -1,9 +1,13 @@
-import { BiChevronLeft, BiDotsHorizontal } from "react-icons/bi";
+import { BiChevronLeft, BiDotsHorizontal, BiSend, BiSmile } from "react-icons/bi";
 import User from "../User";
-import { FaPhone, FaVideo } from "react-icons/fa";
+import { FaMicrophone, FaPhone, FaVideo } from "react-icons/fa";
+import { useState } from "react";
+import Messages from "./Messages";
 
 
 const ChatArea = () => {
+
+    const [message, setMessage] = useState('')
 
     const getUsers = JSON.parse(localStorage.getItem('users'))
     const currentUser = JSON.parse(localStorage.getItem('currentUser'))
@@ -19,8 +23,8 @@ const ChatArea = () => {
     return ( 
         <>
 
-            <div className=" flex items-center justify-between pt-5 pb-2">
-            <div className="flex">
+            <div className=" flex items-center justify-between pt-5 pb-2 pr-2 bg-[#ef91ff7c] rounded-b-xl">
+            <div className="flex text-[#da06ff]">
                     <BiChevronLeft  className="text-5xl"/>
                    {
              getUsers
@@ -33,13 +37,44 @@ const ChatArea = () => {
     
         }
             </div>
-         <div className="text-xl flex items-center gap-5">
-            <FaPhone />
+         <div className="text-xl flex items-center gap-5 text-[#da06ff]">
+            <FaPhone className="rotate-90"/>
             <FaVideo />
             <BiDotsHorizontal />
          </div>
+        </div>
+            <div className="bg-[#6602785b] h-0.5"></div>
+
+            <div className="">
+                <div>
+
+
+
+
+                </div>
+
+               <div className="bg-white absolute bottom-3 left-10 flex items-center gap-2">
+                   <div className="flex items-center justify-between w-[80%] bg-gray-200 rounded-4xl text-2xl p-3">
+                     <BiSmile className="text-[#da06ff]"/>
+                     <input type="text" 
+                     placeholder="Type a message..."
+                     value={message}
+                     onChange={(e)=>{setMessage(e.target.value)}}
+                     className="border-0 text-lg outline-0 w-[70%] m-auto"
+                     />
+                     <BiSend onClick={()=>{
+                        <Messages newMss={message}/>
+                        setMessage('')
+                     }} className="text-[#da06ff]"/>
+                   </div>
+                   <div className="bg-[#b75dc6ec] p-2 rounded-full text-2xl text-white flex items-center">
+                     <FaMicrophone />
+                   </div>
+               </div>
+
+
+
             </div>
-            <hr />
            
         </>
      );
--- default/src/Component/Chat/Messages.jsx
+++ default/src/Component/Chat/Messages.jsx
@@ -1,6 +1,8 @@
 
 
-const Messages = () => {
+const Messages = ({newMss}) => {
+
+    console.log(newMss)
 
     let currentUser = JSON.parse(localStorage.getItem('currentUser'))
                 
@@ -9,11 +11,14 @@ const Messages = () => {
                  let newMessage= { id:crypto.randomUUID(),
                     senderID:currentUser.id,
                     receiverId:currentUser,
-                    createdAt: new Date().toISOString()
+                    createdAt: new Date().toISOString(),
+                    text: newMss
                 }
 
 
            messages.push(newMessage)
+
+           console.log(messages)
 
     return ( 
         <>
